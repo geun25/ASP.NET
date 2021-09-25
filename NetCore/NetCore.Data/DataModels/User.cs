943d6fb662e98490e91da35a711db9fbd8a0a2e8
geun25 <sas0898@naver.com>
2021-09-25 22:39:23 +0900

    .

--- NetCore/NetCore.Data/DataModels/User.cs
+++ NetCore/NetCore.Data/DataModels/User.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,16 +11,33 @@ namespace NetCore.Data.DataModels
     // C# Class => Database Table
     // 멤벼변수 => Table Column
 
+    // 1.Data Annotation
     public class User
     {
+        //Primary Key 지정, 컬럼길이 지정, 컬럼유형 지정
+        [Key, StringLength(50), Column(TypeName = "varchar(50)")]
         public string UserId { get; set; }
 
+        //Not null 지정, 컬럼길이 지정, 컬럼유형 지정
+        [Required, StringLength(100), Column(TypeName = "nvarchar(100)")]
         public string UserName { get; set; }
 
+        [Required, StringLength(320), Column(TypeName = "harchar(320)")]
         public string UserEmail { get; set; }
 
+        [Required, StringLength(130), Column(TypeName = "nvarchar(130)")]
         public string Password { get; set; }
 
-        public DateTime JoinedUtcDate { get; set; } // 가입일자
+        /// <summary>
+        /// 회원 탈퇴여부
+        /// </summary>
+        [Required]
+        public bool IsMembershipWithdrawn { get; set; } 
+       
+        /// <summary>
+        /// 가입일자
+        /// </summary>
+        [Required]
+        public DateTime JoinedUtcDate { get; set; } 
     }
 }
---
+++ NetCore/NetCore.Services/Data/CodeFirstDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCore.Data.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCore.Services.Data
+{
+    public class CodeFirstDbContext : DbContext
+    {
+        //2. Fluent API
+        //생성자 상속
+        public CodeFirstDbContext(DbContextOptions<CodeFirstDbContext> options) : base(options)
+        {
+
+        }
+
+        //DB 테이블 리스트 지정
+        public DbSet<User> Users { get; set; }
+
+        //메서드 상속, 부모클래스에서 OnModelCreating 메서드가 virtual
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //4가지 작업
+            //DB 테이블이름 지정
+            modelBuilder.Entity<User>().ToTable(name: "User");
+
+            //복합키 지정
+            modelBuilder.Entity<UserRoleByUser>().HasKey(c => new { c.UserId, c.RoleId });
+
+            //컬럼 기본값 지정
+            modelBuilder.Entity<User>(e =>
+            {
+                e.Property(c => c.IsMembershipWithdrawn).HasDefaultValue(value: false);
+            });
+
+            //인덱스 지정
+            modelBuilder.Entity<User>().HasIndex(c => new { c.UserEmail });
+        }
+    }
+}
