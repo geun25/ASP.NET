6d5aedf24f8fc9a35dd11cbb025cdd6ebb3786ec
geun25 <sas0898@naver.com>
2021-10-15 17:21:33 +0900

    .

---
+++ Application/Application/Controllers/AlphaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Application.Controllers
+{
+    public class AlphaController : Controller
+    {
+        public string Beta()
+        {
+            return "Alpha Controller Beta Action";
+        }
+    }
+}
---
+++ Application/Application/Controllers/BoardController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Application.Controllers
+{
+    public class BoardController : Controller
+    {
+        public ActionResult List(int? Id)
+        {
+            if (Id == null)
+                return Content("Error Message #1");
+
+            return View();
+        }
+    }
+}
