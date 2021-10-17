20d0486fe9708c3151617c78712dd60a3790f281
geun25 <sas0898@naver.com>
2021-10-17 20:17:41 +0900

    .

---
+++ LibraryApplication/LibraryApplication/Context/LibraryDb.cs
@@ -0,0 +1,11 @@
+﻿using LibraryApplication.Models;
+using System.Data.Entity;
+
+namespace LibraryApplication.Context
+{
+    public class LibraryDb : DbContext
+    {
+        public LibraryDb() : base("name=DBCS") { }
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ LibraryApplication/LibraryApplication/Controllers/HomeController.cs
@@ -0,0 +1,122 @@
+﻿using LibraryApplication.Context;
+using LibraryApplication.Models;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace LibraryApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private LibraryDb db = new LibraryDb();
+
+        public ActionResult Index()
+        {
+            int listCount = 3;
+            int pageNum = 1;
+
+            if (Request.QueryString["page"] != null)
+                pageNum = Convert.ToInt32(Request.QueryString["page"]);
+
+            var books = db.Books.Take(listCount).ToList();
+
+            return View(books);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            
+            Book book = db.Books.Find(id);
+            
+            if (book == null)
+                return HttpNotFound();
+            
+            return View(book);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Book_U,Title,Writer,Summary,Publisher,Published_date")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Books.Add(book);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(book);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            
+            Book book = db.Books.Find(id);
+
+            if (book == null)
+                return HttpNotFound();
+            
+            return View(book);
+        }
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Book_U,Title,Writer,Summary,Publisher,Published_date")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(book).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(book);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            
+            Book book = db.Books.Find(id);
+
+            if (book == null)
+                return HttpNotFound();
+            
+            return View(book);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Book book = db.Books.Find(id);
+
+            db.Books.Remove(book);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                db.Dispose();
+            
+            base.Dispose(disposing);
+        }
+    }
+}
