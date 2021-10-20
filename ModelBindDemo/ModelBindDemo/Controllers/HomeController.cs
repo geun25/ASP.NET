849dbd39db2a067430cff25092a8fb92d99afdee
geun25 <sas0898@naver.com>
2021-10-20 19:19:27 +0900

    Binding 처리

---
+++ ModelBindDemo/ModelBindDemo/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using ModelBindDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModelBindDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Person[] personData =
+        {
+            new Person {PersonId = 1, Name = "김길동", Role = Role.Admin},
+            new Person {PersonId = 2, Name = "홍길동", Role = Role.User},
+            new Person {PersonId = 3, Name = "안길동", Role = Role.User},
+            new Person {PersonId = 4, Name = "오길동", Role = Role.Guest},
+        };
+
+        public ActionResult Index(int id = 1) 
+        {
+            Person person = personData.Where(p => p.PersonId == id).First();
+            return View(person);
+        }
+
+        public ActionResult CreatePerson()
+        {
+            return View(new Person());
+        }
+
+        [HttpPost]
+        public ActionResult CreatePerson(Person model)
+        {
+            return View("Index", model);
+        }
+    }
+}
---
+++ ModelBindDemo/ModelBindDemo/Models/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ModelBindDemo.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Address Addr { get; set; }
+        public Role Role { get; set; }
+    }
+
+    public class Address
+    {
+        public string ZipCode { get; set; }
+        public string Addr1 { get; set; }
+        public string Addr2 { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+
+    public enum Role
+    {
+        Admin,
+        User,
+        Guest 
+    }
+}
