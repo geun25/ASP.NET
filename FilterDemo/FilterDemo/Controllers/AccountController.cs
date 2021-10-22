545c344b1d2c95562a9fca05633a83ef0f3c39fe
geun25 <sas0898@naver.com>
2021-10-22 17:04:40 +0900

    Authorization 필터

---
+++ FilterDemo/FilterDemo/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace FilterDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string userName, string password, string returnUrl)
+        {
+            bool result = FormsAuthentication.Authenticate(userName, password);
+            if(result)
+            {
+                FormsAuthentication.SetAuthCookie(userName, false);
+                return Redirect(returnUrl ?? Url.Action("Index", "Admin"));
+            }
+            else
+            {
+                ModelState.AddModelError("", "인증 실패");
+                return View();
+            }
+        }
+    }
+}
---
+++ FilterDemo/FilterDemo/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FilterDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        public string Index()
+        {
+            return "HomeController의 Index 액션 메소드";
+        }
+    }
+}
