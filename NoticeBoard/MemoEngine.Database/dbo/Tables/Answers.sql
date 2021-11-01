8e912bdb15fae9ae5dbbe3d42c792f637ec66c83
geun25 <sas0898@naver.com>
2021-11-01 12:01:12 +0900

    Answer와 AnswerComment 모델 클래스 생성.

---
+++ NoticeBoard/MemoEngine.Database/dbo/Tables/Answers.sql
@@ -0,0 +1,45 @@
+﻿-- 게시판용 테이블 설계
+CREATE TABLE [dbo].[Answers]
+(
+	[Id]	INT	NOT NULL	PRIMARY KEY	IDENTITY(1,1),	--일련번호
+
+	--5W1H
+	Name			NVARCHAR(25)					NOT NULL,	--이름
+	PostDate		DATETIME	DEFAULT GETDATE()	NOT NULL,	--작성일
+	PostIp			NVARCHAR(15)					NULL,		--작성IP
+	Title			NVARCHAR(150)					NOT NULL,	--제목
+	Content			NVARCHAR(MAX)					NOT NULL,	--내용
+	Category		NVARCHAR(20)	DEFAULT('Free')	NULL,		--카테고리(확장가능)
+
+	-- 기본형 게시판 관련 주요 컬럼
+	Email			NVARCHAR(100)					NULL,		--이메일
+	Password		NVARCHAR(255)					NULL,		--비밀번호
+	ReadCount		INT			DEFAULT 0,						--조회수
+	Encoding		NVARCHAR(20)					NOT NULL,	--인코딩(HTML/Text/Mixed)
+	Homepage		NVARCHAR(100)					NULL,		--홈페이지
+	ModifyDate		DATETIME						NULL,		--수정일
+	ModifyIP		NVARCHAR(15)					NULL,		--수정IP
+	CommentCount	INT			DEFAULT 0,						--댓글수
+	
+	-- 자료실 게시판 관련 주요 컬럼
+	FileName		NVARCHAR(255)					NULL,		--파일명
+	FileSize		INT			DEFAULT 0,						--파일크기
+	DownCount		INT			DEFAULT 0,						--다운수
+
+	--답변형 게시판 관련 주요 컬럼
+	Ref				INT								NOT NULL,	--참조(부모글)
+	Step			INT			DEFAULT 0,						--답변깊이(레벨)
+	RefOrder		INT			DEFAULT 0,						--답변순서
+	AnswerNum		INT			DEFAULT 0,						--답변수
+	ParentNum		INT			DEFAULT 0,						--부모글번호
+
+	-- 기타 확장 기능 관련 주요 컬럼
+	Num				INT								NULL,		--번호(확장...)
+	UID				INT								NULL,		--Users UID
+	UserId			INT								NULL,		--(확장..) 사용자 테이블 Id
+	UserName		NVARCHAR(25)					NULL,		--사용자 아이디
+	DivisionId		INT			DEFAULT 0			NULL,		--서브 카테고리
+	CategoryId		INT	NULL	DEFAULT 0,						--(확장..) 카테고리 테이블 Id
+	BoardId			INT	NULL	DEFAULT 0,						--(확장..) 게시판(Boards) 테이블 Id
+	ApplicationId	INT	NULL	DEFAULT 0						--(확장용) 응용 프로그램 Id
+)
---
+++ NoticeBoard/MemoEngine.Database/dbo/Tables/AnswersComments.sql
@@ -0,0 +1,27 @@
+﻿--댓글 테이블 생성
+CREATE TABLE [dbo].[AnswersComments]
+(
+	Id			INT	IDENTITY(1,1)
+				NOT NULL PRIMARY KEY,	--일련번호
+	ArticledId	INT	NOT NULL,			--해당 Article 게시물 번호(BoardId, ProductId)
+
+	--5W1H
+	Name			NVARCHAR(25)					NOT NULL,	--이름
+	PostDate		DATETIME	DEFAULT GETDATE()	NOT NULL,	--작성일
+	PostIp			NVARCHAR(15)					NULL,		--작성IP
+	Title			NVARCHAR(150)					NOT NULL,	--제목
+	Content			NVARCHAR(MAX)					NOT NULL,	--내용
+	Category		NVARCHAR(20)	DEFAULT('Free')	NULL,		--카테고리(확장가능) -> 공지 자유 자료 사진
+
+	--댓글 관련 주요 컬럼: 레거시
+	Opinion			NVARCHAR(4000)					NULL,		--댓글 내용
+	BoardName		NVARCHAR(50)					NULL,		--게시판 이름(확장) -> Notice, Free, Photo, Data
+	Password		NVARCHAR(255)					NOT NULL,	--댓글 삭제용 암호 
+
+	-- 기타 확장 기능 관련 주요 컬럼
+	Num				INT								NULL,		--번호(확장...)
+	UserId			INT								NULL,		--(확장..) 사용자 테이블 Id
+	CategoryId		INT	NULL	DEFAULT 0,						--(확장..) 카테고리 테이블 Id
+	BoardId			INT	NULL	DEFAULT 0,						--(확장..) 게시판(Boards) 테이블 Id
+	ApplicationId	INT	NULL	DEFAULT 0						--(확장용) 응용 프로그램 Id
+)
